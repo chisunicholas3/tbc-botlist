2bb5ca903eb0b79eb78d98aa9699390ab38ce9af
Floffah <therealfloffah@gmail.com>
2018-04-29 21:18:07 +0100

    Create bots.js

---
+++ bots.js
@@ -0,0 +1,12 @@
+/*
+Please make sure you put your bot in alphabetical order with the others (a mod will do this for you if you don't)
+*/
+var registeredBots = {
+    "HelpBot": {
+        "Author": "ℱ𝓵𝓸𝒇𝒇𝓪𝓱#0001",
+        "Description": "https://raw.githubusercontent.com/Floffah/HelpBot/master/README.md",
+        "Library": "discord.js",
+        "Support Server": "https://discord.gg/RqsHch7",
+        "Website": "https://www.floffah.co.uk/projects/bot"
+    }
+}
