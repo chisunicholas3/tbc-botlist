ec80ec7892cea276f43f194ea49b1ca8ba0dbd58
Floffah <therealfloffah@gmail.com>
2018-04-30 18:29:15 +0100

    Update bots.js

--- bots.js
+++ bots.js
@@ -4,16 +4,20 @@ Please make sure you put your bot in alphabetical order with the others (a mod w
 var registeredBots = {
     "HelpBot": {
         "Author": "ℱ𝓵𝓸𝒇𝒇𝓪𝓱#0001",
-        "Description": "https://raw.githubusercontent.com/Floffah/HelpBot/master/README.md",
         "Library": "discord.js",
         "Support Server": "https://discord.gg/RqsHch7",
-        "Website": "https://www.floffah.co.uk/projects/bot"
+        "Website": "https://www.floffah.co.uk/projects/bot",
+        "ClientID": "439714938760593438",
+        "Invite": "https://discordapp.com/api/oauth2/authorize?client_id=439714938760593438&permissions=1861610727&scope=bot"
+        "Description": "https://raw.githubusercontent.com/Floffah/HelpBot/master/README.md"
     },
     "SpamSpy": {
         "Author": " Fxdee#7875",
-        "Description": "SpamSpy is a bot for everyone.. It helps members entertained and helps admins do ther job | Update 2.0 Coming Very soon",
         "Library": "discord.js",
         "Support Server": "https://discord.gg/xsxjpsX",
-        "Website": "N/A"
+        "Website": "N/A",
+        "ClientID": "432474572370214923",
+        "Invite": "undefined",
+        "Description": "SpamSpy is a bot for everyone.. It helps members entertained and helps admins do ther job | Update 2.0 Coming Very soon"
     }
 }
